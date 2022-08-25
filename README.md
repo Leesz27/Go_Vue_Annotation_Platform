2a84813fe40305cc24017e8fa9c7106329e89a76
lee <522460591@qq.com>
2022-08-25 17:07:03 +0800

    项目框架

---
+++ README.md
@@ -0,0 +1,11 @@
+## 项目说明
+
+### 1. labelstudio-go 后端
+
+
+
+### 2. labelstudio-vue 前端
+
+
+
+## 项目开发中。。。
---
+++ labelstudio-go/main.go
@@ -0,0 +1,12 @@
+/* -*- coding:utf-8 -*-
+ * @Author: QD_Lee
+ * @Datetime: 2022/8/25 17:02
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("hello world")
+}
